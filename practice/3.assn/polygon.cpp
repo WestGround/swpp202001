b320b15fb3ada6b5c6c98a04d6497975838dcc72
WestGround <dalsan2113@snu.ac.kr>
2020-04-07 00:05:43 +0900

    Fix signed integer overflow in polygon

--- practice/3.assn/polygon.cpp
+++ practice/3.assn/polygon.cpp
@@ -7,17 +7,28 @@ uint64_t area(vector<pair<int, int>> &points) {
   // This code has signed overflows. :)
   // Please fix this so it correctly evaluates area..!
   int64_t total = 0;
+  uint64_t area = 0;
   size_t n = points.size();
+  int remain = 0;
 
   for (unsigned i = 0; i < n; i++) {
     unsigned j = (i + 1) % n;
-    int x_i = points[i].first;
-    int y_i = points[i].second;
-    int x_j = points[j].first;
-    int y_j = points[j].second;
-    total += (x_i * (y_j - y_i) - y_i * (x_j - x_i));
+    int64_t x_i = points[i].first;
+    int64_t y_i = points[i].second;
+    int64_t x_j = points[j].first;
+    int64_t y_j = points[j].second;
+
+    area = (uint64_t)(x_i * (y_j - y_i) - y_i * (x_j - x_i));
+    total += area/2;
+
+    // truncation occurs by rounding off
+    if( (area%2 == 1) && (remain == 1) ) {
+      total++;
+      remain = 0;
+    }
+    else if(area%2 == 1)
+      remain = 1;
   }
 
-  total /= 2;
   return total;
 }
