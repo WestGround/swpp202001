979f0db4a94c0558fd42450ef64318e210bd048c
WestGround <dalsan2113@snu.ac.kr>
2020-05-03 16:29:06 +0900

    Implement google test for getBlockType

--- practice/5.assn/allocTypeTest.cpp
+++ practice/5.assn/allocTypeTest.cpp
@@ -22,24 +22,49 @@ TEST(AllocTypeTest, MyTest) {
   Function *MallocF = Function::Create(MallocFTy, Function::ExternalLinkage,
                                        "malloc", *M);
 
-  // Please fill @test() using IRBuilder to TestF.
-  // The created program should contain at least:
-  //   - One call instruction to malloc
-  //     (hint: 'CreateCall(MallocF, { (size argument) }, "p")').
-  //   - One alloca instruction
-  //   - One bitcast
-  //   - One getelementptr
+  BasicBlock *bbEntry = BasicBlock::Create(Context, "entry", TestF);
+  IRBuilder<> entry(bbEntry);
 
-  // Please write 4 EXPECT_* calls.
-  // ex) EXPECT_EQ(getBlockType(p), AllocType::HEAP);
-  // There should be at least:
-  //   - One EXPECT_* check to AllocType::HEAP
-  //   - One EXPECT_* check to AllocType::STACK
-  //   - One EXPECT_* check to AllocType::UNKNOWN
+  // arr : %arr = malloc(8)
+  auto *n = ConstantInt::get(Type::getInt32Ty(Context), 8);
+  auto *arr = entry.CreateCall(MallocF, {n}, "arr");
 
-  // A dummy EXPECT_EQ.
-  // Please delete this line after you finish your implementation.
-  EXPECT_EQ(1234, 5678);
+  // ep1 : %ep1 = getelementptr i8, i8* %arr, i32 0
+  // e1 : %e1 = load i8, i8* %ep1
+  // ep2 : %ep2 = getelementptr i8, i8* %arr, i32 1
+  // e2 : %e2 = load i8, i8* %ep2
+  auto *ep1 = entry.CreateGEP(arr, ConstantInt::get(Type::getInt32Ty(Context), 0), "ep1");
+  auto *e1 = entry.CreateLoad(ep1, "e1");
+  auto *ep2 = entry.CreateGEP(arr, ConstantInt::get(Type::getInt32Ty(Context), 1), "ep2");
+  auto *e2 = entry.CreateLoad(ep2, "e2");
+
+  // s : %sum = add i8 %e1, %e2
+  // avg : %avg = udiv i8 %sum, 2
+  // avg64 : %avg32 = bitcast i8 %avg to i32
+  auto *s = entry.CreateAdd(e1, e2, "sum");
+  auto *avg = entry.CreateUDiv(s, ConstantInt::get(Type::getInt8Ty(Context), 2), "avg");
+  auto *avg32 = entry.CreateBitCast(avg, Type::getInt32Ty(Context), "avg32");
+
+  // p : %p = alloca i32
+  // st : store i32 %avg32, i32* %p
+  auto *p = entry.CreateAlloca(Type::getInt32Ty(Context), ConstantInt::get(Type::getInt32Ty(Context), 1), "p");
+  auto *st = entry.CreateStore(avg32, p);
+
+  BasicBlock *bbExit = BasicBlock::Create(Context, "exit", TestF);
+  entry.CreateBr(bbExit);
+  IRBuilder<>(bbExit).CreateRetVoid();
+
+  FunctionAnalysisManager FAM;
+  PassBuilder PB;
+  PB.registerFunctionAnalyses(FAM);
+  auto AARes = FAM.getResult<BasicAA>(*TestF);
+
+  EXPECT_EQ(getBlockType(n), UNKNOWN);
+  EXPECT_EQ(getBlockType(arr), HEAP);
+  EXPECT_EQ(getBlockType(ep1), HEAP);
+  EXPECT_EQ(getBlockType(avg32), UNKNOWN);
+  EXPECT_EQ(getBlockType(p), STACK);
+  EXPECT_NE(getBlockType(st), HEAP);
 }
 
 
